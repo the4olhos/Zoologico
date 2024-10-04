f33282e73c1863ca481393b7d5156a7c3dc19fef
Fussuma Murilo <murilofussuma@outlook.com>
2024-10-03 21:50:05 -0300

    criação da sub-classe mamífero

--- src/Classes/Animal.java
+++ src/Classes/Animal.java
@@ -53,4 +53,36 @@ public class Animal {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    public static class Mamífero extends Animal{
+
+        public boolean temPelo;
+
+        public Mamifero(this.temPelo){
+            super(nome, especie, idade);
+            this.temPelo = temPelo;
+        }
+
+        @Override
+        public void emitirSom() {
+            System.out.println("Emitindo sons de animal mamífero");
+        }
+
+        public String getTemPelo(this.temPelo){
+            if (temPelo) {
+                return "Sim";
+            } else {
+                return "Não";
+            }
+        }
+
+        @Override
+        public String toString(){
+            return "Informações do Mamífero"
+                    + "Nome: " + this.getNome()
+                    + "Espécie: " + this.getEspecie()
+                    + "Idade: " + this.getIdade()
+                    + "Tem pelo: " + getTemPelo() ;
+        }
+    }
 }
---
+++ src/Classes/Mamifero.java
@@ -0,0 +1,33 @@
+package Classes;
+
+public class Mamifero extends Animal {
+
+    private boolean temPelo;
+
+    public Mamifero(String nome, String especie, int idade, boolean temmPelo){
+        super(nome, especie, idade);
+        this.temPelo = temPelo;
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Emitindo sons de animal mamífero");
+    }
+
+    public String getTemPelo(){
+        if (temPelo) {
+            return "Sim";
+        } else {
+            return "Não";
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Informações do Mamífero:"
+                + "Nome: " + this.getNome()
+                + "Espécie: " + this.getEspecie()
+                + "Idade: " + this.getIdade()
+                + "Tem pelo: " + getTemPelo() ;
+    }
+}
