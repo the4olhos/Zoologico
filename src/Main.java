9d893282fc563f5eddbb0f52de401103ecec4372
Samuca Sozua <158528391+devsamuca@users.noreply.github.com>
2024-10-04 09:41:19 -0300

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Classes.Animal;
 import Classes.SubClasses.Ave;
+import Classes.SubClasses.Mamifero;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Quantidade de Animais a serem cadastrados
+        //Quantidade de Aves a serem cadastrados (Definido pelo usuarios)
         int quantidadeAve;
 
         //Variavel para captar dados do usuario
@@ -15,26 +16,127 @@ public class Main {
         System.out.print("Quantas Aves voce deseja cadastrar: ");
         quantidadeAve = sc.nextInt();
 
-        //Criando o array com a quantidade de aves solicitada
-        Animal[] ave = new Ave[quantidadeAve];
+        if (quantidadeAve == 0) {
+            System.out.println("A quantidade nao pode ser 0");
+            System.out.print("Quantas Aves voce deseja cadastrar: ");
+            quantidadeAve = sc.nextInt();
+        } else {
+            //Criando o array com a quantidade de aves solicitada
+            Animal[] ave = new Ave[quantidadeAve];
 
-        for (int i = 0; i < quantidadeAve; i++) {
-            System.out.print("Nome: ");
-            String nome = sc.next();
-            System.out.print("Especie: ");
-            String especie = sc.next();
-            System.out.print("Idade: ");
-            int idade = sc.nextInt();
-            System.out.print("Pode Voar Sim ou Não: ");
-            String podeVoar = sc.next();
-            boolean voar;
-            if(podeVoar.equals("Sim")) {
-                 voar = true;
-            } else {
-                 voar = false;
-            }
-            ave[i] = new Ave(nome, especie,idade,voar);
-            System.out.println(ave[i].toString());
+            ///Loop para criar os objetos
+            for (int i = 0; i < quantidadeAve; i++) {
+                System.out.print("Nome: ");
+                //Capturando dado do usuario
+                String nomeAve = sc.next();
+                System.out.print("Especie: ");
+                //Capturando dado do usuario
+                String especieAve = sc.next();
+                System.out.print("Idade: ");
+                //Capturando dado do usuario
+                int idadeAve = sc.nextInt();
+                System.out.print("Pode Voar Sim ou Não: ");
+                //Capturando dado do usuario
+                String podeVoar = sc.next();
+                boolean voar;
+                if (podeVoar.equals("Sim")) {
+                    voar = true;
+                } else {
+                    voar = false;
+                }
+                //Objeto criado
+                ave[i] = new Ave(nomeAve, especieAve, idadeAve, voar);
+                System.out.println(ave[i].toString());
         }
- }
+
+
+
+
+            //Quantidade de Mamiferos a serem cadastrados (Definido pelo usuarios)
+            int quantidadeMamiferos;
+
+            //Capturando dado do usuario
+            System.out.print("Quantos Mamiferos você deseja cadastrar: ");
+            quantidadeMamiferos = sc.nextInt();
+
+            if (quantidadeMamiferos == 0) {
+                System.out.println("A quantidade nao pode ser 0");
+                System.out.print("Quantos Mamiferos voce deseja cadastrar: ");
+                quantidadeMamiferos = sc.nextInt();
+            } else {
+                //Criando o array com a quantidade de mamiferos solicitada
+                Animal[] mamifero = new Mamifero[quantidadeMamiferos];
+
+                ///Loop para criar os objetos
+                for (int j = 0; j < quantidadeMamiferos; j++) {
+                    System.out.print("Nome: ");
+                    //Capturando dado do usuario
+                    String nomeMamifero = sc.next();
+                    System.out.print("Especie: ");
+                    //Capturando dado do usuario
+                    String especieMamifero = sc.next();
+                    System.out.print("Idade: ");
+                    //Capturando dado do usuario
+                    int idadeMamifero = sc.nextInt();
+                    System.out.print("Pode Voar Sim ou Não: ");
+                    //Capturando dado do usuario
+                    String temPelo = sc.next();
+                    boolean pelo;
+                    if (temPelo.equals("Sim")) {
+                        pelo = true;
+                    } else {
+                        pelo = false;
+                    }
+                    //Objeto criado
+                    ave[j] = new Ave(nomeMamifero, especieMamifero, idadeMamifero, pelo);
+                    System.out.println(mamifero[j].toString());
+            }
+
+
+
+
+
+                //Quantidade de Repteis a serem cadastrados (Definido pelo usuarios)
+                int quantidadeReptil;
+
+                //Capturando dado do usuario
+                System.out.print("Quantos Repteis você deseja cadastrar: ");
+                quantidadeReptil = sc.nextInt();
+
+                if (quantidadeReptil == 0) {
+                    System.out.println("A quantidade nao pode ser 0");
+                    System.out.print("Quantos Repteis você deseja cadastrar: ");
+                    quantidadeReptil = sc.nextInt();
+                } else{
+                    //Criando o array com a quantidade de mamiferos solicitada
+                    Animal[] reptil = new Mamifero[quantidadeReptil];
+
+                    ///Loop para criar os objetos
+                    for (int k = 0; k < quantidadeReptil; k++) {
+                        System.out.print("Nome: ");
+                        //Capturando dado do usuario
+                        String nomeReptil = sc.next();
+                        System.out.print("Especie: ");
+                        //Capturando dado do usuario
+                        String especieReptil = sc.next();
+                        System.out.print("Idade: ");
+                        //Capturando dado do usuario
+                        int idadeReptil = sc.nextInt();
+                        System.out.print("Pode Voar Sim ou Não: ");
+                        //Capturando dado do usuario
+                        String eVenenoso = sc.next();
+                        boolean veneno;
+                        if (eVenenoso.equals("Sim")) {
+                            veneno = true;
+                        } else {
+                            veneno = false;
+                        }
+                        //Objeto criado
+                        ave[k] = new Ave(nomeReptil, especieReptil, idadeReptil, veneno);
+                        System.out.println(mamifero[k].toString());
+                    }
+                }
+            }
+        }
+    }
 }
